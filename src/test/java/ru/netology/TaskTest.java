563a83702aef1e0f5f39983791454ee1b6ea83fb
eleonora <rahovskaaeleonoraf@gmail.com>
2023-10-22 18:50:32 +0300

    Code revision

---
+++ src/test/java/ru/netology/TaskTest.java
@@ -0,0 +1,46 @@
+package ru.netology;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TaskTest {
+
+    @Test
+    public void testMatchesSimpleTask() {
+        SimpleTask simpleTask = new SimpleTask(5, "Приготовить обед");
+
+        boolean actual = simpleTask.matches("Суп");
+        boolean expected  = false;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testMatchesEpic() {
+        String[] subtasks = { "Сметана", "Творог", "Батон" };
+        Epic epic = new Epic(55, subtasks);
+
+        boolean actual = epic.matches("Сметана");
+        boolean expected = true;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMatchesMeeting() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        boolean actual = meeting.matches("Во вторник после обеда");
+        boolean expected = false;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+
+}
